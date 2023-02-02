0b431b75e6148bc4c977ce12c3a8e91528b29768
silversirp <silver.sirp@khk.ee>
2023-02-02 11:28:46 +0200

    Splash ekraan

---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { 
+    Text, 
+    View, 
+    Image 
+} from "react-native";
+import { styles } from "./styles";
+
+const Splash = () => {
+    return (
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
+            <Text style={styles.title}>You’ll Find </Text>
+            <Text style={[styles.title, styles.innerTitle]}>All you need </Text>
+            <Text style={styles.title}>Here!</Text>
+        </View>
+        
+    )
+}
+export default Splash;
