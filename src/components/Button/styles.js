61bdc2c6b024a746c6d7cae364e126390fdfe36c
silversirp <silver.sirp@khk.ee>
2023-02-02 15:33:26 +0200

    Create Splash screen

---
+++ src/components/Button/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#4F63AC',
+        paddingVertical: 20,
+        paddingHorizontal: 8,
+        borderRadius: 8,
+        width: '100%'
+        // height: 60,
+        // position: "absolute",
+        // right: 36,
+        //top: '74.88%',
+        // bottom: -70
+    },
+
+    title: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+})
--- src/screens/auth/Splash/styles.js
+++ src/screens/auth/Splash/styles.js
@@ -1,9 +1,22 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        // borderWidth: 1
+    },
+
     image: {
         width: '100%',
         height: 200
+    },
+
+    titleContainer: {
+        marginVertical: 54
     },
 
     title: {
@@ -17,7 +30,23 @@ export const styles = StyleSheet.create({
         textDecorationLine: 'underline'
     },
 
-    container: {
 
+    footerText: {
+        backgroundColor: '#FFFFFF',
+        color: '#4F63AC',
+        fontSize: 16,
+        fontStyle: "normal",
+        lineHeight: 20,
+        width: '100%',
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        // borderWidth: 1,
+        marginTop: 30,
+        // height: 60,
+        // position: "absolute",
+        // right: 36,
+        // bottom: -160,
+        textAlign: "center"
     }
+
 })
