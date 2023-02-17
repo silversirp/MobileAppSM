e94b6cbe48d749f8fb571d5ed4cad3829906e35f
Silver Sirp <silver.sirp@khk.ee>
2023-02-17 12:55:45 +0200

    Create homepage screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import { Pressable, Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import Input from "../Input";
 
-const Header = ({title, onBackPress, onLogout, showBack, showSearch, showLogout}) => {
+const Header = ({title, onBackPress, onLogout, showBack, showSearch, showLogout, onSearchKeyword, keyword}) => {
     const [showSearchInput, setShowSearchInput] = useState(false)
 
     const onSearch = () => {
@@ -34,7 +34,7 @@ const Header = ({title, onBackPress, onLogout, showBack, showSearch, showLogout}
             </View>
             {
                 showSearchInput ? (
-                    <Input placeholder="Type your keyword" />
+                    <Input onChangeText={onSearchKeyword} value={keyword} placeholder="Type your keyword" />
                 ) : null
             }
         </View>
--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -10,16 +10,23 @@ import { products } from "../../../data/products";
 
 const Home = () => {
     const [selectedCategory, setSelectedCategory] = useState()
+    const [keyword, setKeyword] = useState()
     const [selectedProducts, setSelectedProducts] = useState(products)
 
     useEffect(() => {
-        if(selectedCategory){
+        if(selectedCategory && !keyword){
             const updatedSelectedProducts = products.filter((product) => product?.category === selectedCategory)
             setSelectedProducts(updatedSelectedProducts)
-        } else { 
+        } else if(selectedCategory && keyword) {
+            const updatedSelectedProducts = products.filter((product) => product?.category === selectedCategory && product?.title?.toLowerCase().includes(keyword.toLowerCase()))
+            setSelectedProducts(updatedSelectedProducts)
+        } else if(!selectedCategory && keyword) {
+            const updatedSelectedProducts = products.filter((product) => product?.title?.toLowerCase().includes(keyword?.toLowerCase()))
+            setSelectedProducts(updatedSelectedProducts)
+        } else if(!keyword && !selectedCategory) {
             setSelectedProducts(products)
         }
-    }, [selectedCategory])
+    }, [selectedCategory, keyword])
 
     const renderCategoryItem = ({item}) => {
         console.log('item =>', item)
@@ -41,7 +48,7 @@ const Home = () => {
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Header showSearch={true} title="Find All You Need" />
+                <Header showSearch={true} onSearchKeyword={setKeyword} keyword={keyword} title="Find All You Need" />
                 <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) => String(index)} />
                 <FlatList numColumns={2} data={selectedProducts} renderItem={renderProductItem} keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}}/>} />
             </View>
