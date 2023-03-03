9e7ab407f9937d8cc1960dea16a2af2918b16292
Silver Sirp <silver.sirp@khk.ee>
2023-03-03 11:16:42 +0200

    Create profile screen

--- src/screens/app/CreateListing/index.js
+++ src/screens/app/CreateListing/index.js
@@ -1,34 +1,77 @@
 import React, {useState} from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image, ViewBase, Pressable, ActivityIndicator, KeyboardAvoidingView, ScrollView } from "react-native";
 import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../../components/Header";
+import Button from "../../../components/Button";
+import { categories } from "../../../data/categories";
 
 import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import Input from "../../../components/Input";
 
 const CreateListing = ({navigation}) => {
-    //const [images, setImages] = useState([])
+    const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [values, setValues] = useState({})
+
+    console.log('values => ', values)
     
     const goBack = () => {
         navigation.goBack()
     }
 
     const uploadNewImage = async () => {
+        setLoading(true)
         const result = await launchImageLibrary()
-        console.log('help', result)
-      //  setImages(list => ([...list, result?.assets]))
+        console.log(result)
+        if(result?.assets?.length) {
+            setImages(list => ([...list, ...result?.assets]))
+            setLoading(false)
+        }
+        console.log(images)
+    }
+
+    const onDeleteImage = (image) => {
+        setImages((list) => {
+            const filteredImages = list.filter((img) => img?.fileName !== image?.fileName)
+            return filteredImages
+        })
+    }
+
+    const onChange = (value, key) => {
+        setValues((val) => ({...val, [key]: value}))
     }
 
     return (
         <SafeAreaView style={{flex: 1}}>
+            <KeyboardAvoidingView behavior="position">
             <Header showBack={true} onBackPress={goBack} title="Create a new listing"/>
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <Text style={styles.sectionTitle}>Upload photos</Text>
-                <TouchableOpacity onPress={uploadNewImage}>
-                    <Text>+</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
+                <View style={styles.imageRow}>
+                    <TouchableOpacity style={styles.uploadContainer} onPress={uploadNewImage}>
+                        <View style={styles.uploadCircle}>
+                            <Text style={styles.uploadPlus}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                    {images?.map(image => (
+                        <View key={image?.fileName} style={styles.imageContainer} >
+                            <Image style={styles.image} source={{uri: image?.uri}}/>
+                            <Pressable hitSlop={20} onPress={() => onDeleteImage(image)} >
+                                <Image style={styles.delete} source={require('../../../assets/close.png')} />
+                            </Pressable>
+                        </View>
+                    ))}
+                    {loading ? (<ActivityIndicator/>) : null}
+                </View>
+                <Input label="Title" placeholder="Listing Title" value={values.title} onChangeText={(v) => onChange(v, 'title')} />
+                <Input label="Category" placeholder="Select the category" value={values.category} onChangeText={(v) => onChange(v, 'category')} type='picker' options={categories} />
+                <Input label="Price" placeholder="Enter price in USD" value={values.price} onChangeText={(v) => onChange(v, 'price')} keyboardType="numeric" />
+                <Input style={styles.textarea} label="Description" placeholder="Tell us more..." value={values.description} onChangeText={(v) => onChange(v, 'description')} multiline />
+                <Button style={styles.button} title="Submit"/>
+            </ScrollView>
+            </KeyboardAvoidingView>
+                    </SafeAreaView>
     )
 }
 
--- src/screens/app/CreateListing/styles.js
+++ src/screens/app/CreateListing/styles.js
@@ -11,6 +11,67 @@ export const styles = StyleSheet.create({
    fontSize: 14,
    color: colors.blue,
    marginBottom: 16
+   },
+
+   image: {
+      width: 100,
+      height: 100,
+      borderRadius: 8,
+      marginRight: 8,
+      marginBottom: 8,
+      marginTop: 8
+   },
+
+   uploadContainer: {
+      width: 100,
+      height: 100,
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: colors.grey,
+      borderStyle: 'dotted',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 8,
+   },
+
+   uploadCircle: {
+      width: 32,
+      height: 32,
+      borderRadius: 20,
+      backgroundColor: colors.lightgrey,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+
+   uploadPlus: {
+      color: colors.white,
+      fontSize: 28,
+      marginTop: -4
+   },
+
+   imageRow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      flexWrap: 'wrap',      
+   },
+
+   imageContainer: {
+      flexDirection: 'row',
+   },
+
+   delete: {
+      width: 16,
+      height: 16,
+      position: 'absolute',
+      right: 0,
+      top: 2
+   },
+
+   textarea: {
+      minHeight: 120,
+      paddingTop: 16
    }
   
 
